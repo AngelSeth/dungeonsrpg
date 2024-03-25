7656e02954beea25f464ed49dee80d5a236b31a0
AngelSeth <sethmoosepic@gmail.com>
2024-03-24 20:46:16 -0700

    working on mixin, ignore this commit

---
+++ src/main/java/mythcore/dungeons/mixin/PlayerHudMixin.java
@@ -0,0 +1,17 @@
+package mythcore.dungeons.mixin;
+
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public class PlayerHudMixin {
+    @Inject(at = @At("HEAD"), method = "init()V")
+    private void init(CallbackInfo info) {
+        System.out.println("This line is printed by an example mod mixin!");
+    }
+}
