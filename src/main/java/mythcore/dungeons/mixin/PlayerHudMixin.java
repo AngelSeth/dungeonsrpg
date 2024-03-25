d5a99cafdb9a1d5b23cc346592f410723aa1c407
AngelSeth <sethmoosepic@gmail.com>
2024-03-24 21:35:40 -0700

    fixed build.gradle and dungeonsrpg.mixins.json, healthbar is no longer rendered now just need to create the replacement

--- src/main/java/mythcore/dungeons/mixin/PlayerHudMixin.java
+++ src/main/java/mythcore/dungeons/mixin/PlayerHudMixin.java
@@ -1,18 +1,33 @@
 package mythcore.dungeons.mixin;
 
+import mythcore.dungeons.DungeonsRPG;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.util.Window;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
-public class PlayerHudMixin {
-    @Inject(at = @At("HEAD"), method = "render(Lnet/minecraft/client/util/math/MatrixStack;F)V", cancellable = true)
-    private void init(CallbackInfo info) {
-        System.out.println("This line is printed by an example mod mixin!");
+public abstract class PlayerHudMixin {
+    private Identifier textRenderer;
+
+    @Shadow public abstract TextRenderer getTextRenderer();
+
+    private static final Identifier TEXTURE = new Identifier(DungeonsRPG.MOD_ID, "textures/gui/stat_screen.png");
+    @Inject(method = "renderHealthBar", at = @At("HEAD"), cancellable = true)
+    private void renderHealthBar(DrawContext context, PlayerEntity playerEntity, int x, int y, int lines, int regeneratingHeartIndex, float maxHealth, int lastHealth, int health, int absorption, boolean blinking, CallbackInfo info) {
+        // Prevent the default HUD from rendering
+        info.cancel();
+        // Your custom rendering code here
     }
+
 }
